15bbce4f69bebc5eab5fa40ab127ff2a17fd5307
cleave owhiroro <owhiroroeghele@gmail.com>
2022-12-09 20:26:03 +0100

    implement auth

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,22 +1,7 @@
 import { useEffect } from "react";
 
-import { Row, Col, Breadcrumb, Button, Input } from "antd";
-
-import { SearchOutlined } from "@ant-design/icons";
-
-import { NavLink, Link } from "react-router-dom";
+import { Row, Col, Button } from "antd";
 import { useAppSelector } from "redux/hooks";
-
-const profile = [
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" key={0}>
-        <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10ZM12 7C12 8.10457 11.1046 9 10 9C8.89543 9 8 8.10457 8 7C8 5.89543 8.89543 5 10 5C11.1046 5 12 5.89543 12 7ZM9.99993 11C7.98239 11 6.24394 12.195 5.45374 13.9157C6.55403 15.192 8.18265 16 9.99998 16C11.8173 16 13.4459 15.1921 14.5462 13.9158C13.756 12.195 12.0175 11 9.99993 11Z"
-            fill="#111827"
-        ></path>
-    </svg>,
-];
 
 const toggler = [
     <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" key={0}>
@@ -24,7 +9,7 @@ const toggler = [
     </svg>,
 ];
 
-function Header({ name, subName, onPress }) {
+function Header({ subName, onPress }) {
 
     useEffect(() => window.scrollTo(0, 0));
 
@@ -34,14 +19,7 @@ function Header({ name, subName, onPress }) {
         <>
             <Row gutter={[24, 0]}>
                 <Col span={24} md={6}>
-                    <Breadcrumb>
-                        <Breadcrumb.Item>
-                            <NavLink to="/">Pages</NavLink>
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item style={{ textTransform: "capitalize" }}>
-                            {name.replace("/", "")}
-                        </Breadcrumb.Item>
-                    </Breadcrumb>
+                    <h1>Welcome, <span>{user?.firstName}  {user?.lastName}</span></h1>
                     <div className="ant-page-header-heading">
                         <span className="ant-page-header-heading-title" style={{ textTransform: "capitalize" }}>
                             {subName.replace("/", "")}
@@ -52,11 +30,6 @@ function Header({ name, subName, onPress }) {
                     <Button type="link" className="sidebar-toggler" onClick={() => onPress()}>
                         {toggler}
                     </Button>
-                    <Link to="/profile" className="btn-sign-in">
-                        {profile}
-                        <span>{user?.firstName}  {user?.lastName}</span>
-                    </Link>
-                    <Input className="header-search" placeholder="Type here..." prefix={<SearchOutlined />} />
                 </Col>
             </Row>
         </>
